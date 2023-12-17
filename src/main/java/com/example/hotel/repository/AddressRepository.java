2e15aba499311f0a1367607cfd8056e9d9b1d4d3
YZCUS <43754173+YZCUS@users.noreply.github.com>
2023-12-17 15:07:01 -0500

    change preset data

--- src/main/java/com/example/hotel/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.hotel.model.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- src/main/java/com/example/hotel/repository/DiscountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotel.repository;
-
-import com.example.hotel.model.Discount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiscountRepository extends JpaRepository<Discount, Long> {
-}
